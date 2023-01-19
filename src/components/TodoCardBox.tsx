d5ccca733c98d6801131467b8bccbf82ad2dd7ef
Jeong <hj08hn@gmail.com>
2023-01-19 09:22:01 +0900

    add props in TodoCardBox.tsx
    - TodoCardBox.tsx에 props를 import하여 타입을 지정합니다
    
    @Hah-nna

--- src/components/TodoCardBox.tsx
+++ src/components/TodoCardBox.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoCard from "./TodoCard";
+import { props } from "../api";
 
-const TodoCardBox = ({ isActive, todos, setTodos }) => {
+const TodoCardBox = ({ isActive, todos, setTodos }: props) => {
   return (
     <div>
       <div>{isActive ? "Work" : "Done"}</div>
+
       {todos
         .filter((item) => item.isDone === !isActive)
         .map((item) => {
