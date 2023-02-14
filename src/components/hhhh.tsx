ff967364de56be434541dea4b0227674f1d9fba5
Jeong <hj08hn@gmail.com>
2023-02-15 00:41:50 +0900

    test

---
+++ src/components/hhhh.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const hhhh = (props: Props) => {
+  return <div>hhhh</div>;
+};
+
+export default hhhh;
---
+++ src/components/hi.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const hi = (props: Props) => {
+  return <div>okokoko</div>;
+};
+
+export default hi;
